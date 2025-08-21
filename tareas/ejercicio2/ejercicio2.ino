8415351c2878c0f63783f616a1e2d7d7e9925c75
Marcelo767n <luismarcelo767n@gmail.com>
2025-08-21 00:09:09 -0400

    tarea 1 finalizada

--- tareas/ejercicio2/ejercicio2.ino
+++ tareas/ejercicio2/ejercicio2.ino
@@ -1,54 +1,37 @@
-//definimos los pines con las macros 
-#define NUM_LEDS   15       
-#define INTERVALO  500       
-#define LED1   2
-#define LED2   3
-#define LED3   4
-#define LED4   5
-#define LED5   6
-#define LED6   7
-#define LED7   8
-#define LED8   9
-#define LED9   10
-#define LED10  11
-#define LED11  12
-#define LED12  13
-#define LED13  A0
-#define LED14  A1
-#define LED15  A2
+// Ejercicio 2: 15 LEDs con intervalo de 0.5 segundos
 
 void setup() {
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
-  pinMode(LED4, OUTPUT);
-  pinMode(LED5, OUTPUT);
-  pinMode(LED6, OUTPUT);
-  pinMode(LED7, OUTPUT);
-  pinMode(LED8, OUTPUT);
-  pinMode(LED9, OUTPUT);
-  pinMode(LED10, OUTPUT);
-  pinMode(LED11, OUTPUT);
-  pinMode(LED12, OUTPUT);
-  pinMode(LED13, OUTPUT);
-  pinMode(LED14, OUTPUT);
-  pinMode(LED15, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(A0, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED1, HIGH); delay(INTERVALO);
-  digitalWrite(LED2, HIGH); delay(INTERVALO);
-  digitalWrite(LED3, HIGH); delay(INTERVALO);
-  digitalWrite(LED4, HIGH); delay(INTERVALO);
-  digitalWrite(LED5, HIGH); delay(INTERVALO);
-  digitalWrite(LED6, HIGH); delay(INTERVALO);
-  digitalWrite(LED7, HIGH); delay(INTERVALO);
-  digitalWrite(LED8, HIGH); delay(INTERVALO);
-  digitalWrite(LED9, HIGH); delay(INTERVALO);
-  digitalWrite(LED10, HIGH); delay(INTERVALO);
-  digitalWrite(LED11, HIGH); delay(INTERVALO);
-  digitalWrite(LED12, HIGH); delay(INTERVALO);
-  digitalWrite(LED13, HIGH); delay(INTERVALO);
-  digitalWrite(LED14, HIGH); delay(INTERVALO);
-  digitalWrite(LED15, HIGH); delay(INTERVALO);
+  digitalWrite(2, HIGH); delay(500);
+  digitalWrite(3, HIGH); delay(500);
+  digitalWrite(4, HIGH); delay(500);
+  digitalWrite(5, HIGH); delay(500);
+  digitalWrite(6, HIGH); delay(500);
+  digitalWrite(7, HIGH); delay(500);
+  digitalWrite(8, HIGH); delay(500);
+  digitalWrite(9, HIGH); delay(500);
+  digitalWrite(10, HIGH); delay(500);
+  digitalWrite(11, HIGH); delay(500);
+  digitalWrite(12, HIGH); delay(500);
+  digitalWrite(13, HIGH); delay(500);
+  digitalWrite(A0, HIGH); delay(500);
+  digitalWrite(A1, HIGH); delay(500);
+  digitalWrite(A2, HIGH); delay(500);
 }
